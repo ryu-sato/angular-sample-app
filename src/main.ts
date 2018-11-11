4e3aaf4a11c8b265a3a0466dbdf84fed0f612cca
Ryu Sato <ryu@weseek.co.jp>
2018-11-11 22:40:55 +0900

    日本語設定に対応

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,30 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+// 日本語の場合のみ日本語に翻訳する
+declare const require;
+const translationProviders = (() => {
+  // ブラウザの言語を取得
+  const browserLanguage: string = (() => {
+    if (navigator.languages.length > 0) {
+      return navigator.languages[0];
+    }
+    if (navigator.language) {
+      return navigator.language;
+    }
+    return 'en';
+  })();
+
+  // 日本語だったら翻訳設定
+  if (browserLanguage.match(/^ja$|^ja-/)) {
+    const translations = require('raw-loader!./locale/messages.ja.xlf');
+    return [
+      {provide: TRANSLATIONS, useValue: translations},
+      {provide: TRANSLATIONS_FORMAT, useValue: 'xlf'},
+    ];
+  }
+})();
+
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [].concat(translationProviders) // 翻訳が必要な場合のみ、翻訳に必要なプロバイダーを追加する
+}).catch(err => console.log(err));
